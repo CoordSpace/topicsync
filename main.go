8b9b82763cb4fee037a04e5824bda8c3f98c935d
Chris Earley <chris@coord.space>
2019-08-07 23:38:52 -0500

    removed bimap for vanilla map, added struct to manage the singular state for the clients and handler methods.

--- main.go
+++ main.go
@@ -1,42 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"sync"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
+	//"time"
 	"github.com/bwmarrin/discordgo"
 	"github.com/lrstanley/girc"
 	"github.com/spf13/viper"
 )
 
-type ircClient struct {
-	client		*girc.Client
-	isReady		bool
+type Context struct {
+	irc *girc.Client
+	discord *discordgo.Session
+	channelMap 	map[string]string 
+	wg *sync.WaitGroup
 }
 
-type discordClient struct {
-	session		*discordgo.Session
-	isReady		bool
-}
-
-type topicInfo struct {
-	topic		string
-	timeSet		time.Time
-	mutex		sync.Mutex
-}
-
-var (
-	i	*ircClient
-	d	*discordClient
-	t	*topicInfo
-)
-
-func StartIRC() {
-	log.Print("Starting up IRC...")
+func createIRC() *girc.Client {
+	// create a new IRC client using the config information
 	config := girc.Config{
 		Server: viper.GetString("irc.server"),
 		Port:   viper.GetInt("irc.port"),
@@ -44,66 +28,77 @@ func StartIRC() {
 		User:   viper.GetString("irc.user"),
 		Name:   viper.GetString("irc.name"),
 	}
-
 	// Setup our IRC client instance
-	i := &ircClient {
-		client: girc.New(config),
-		isReady: false,
-	}
+	return girc.New(config)
+}
 
-	log.Print("Setting up IRC Handlers...")
-	i.client.Handlers.Add(girc.CONNECTED, func(c *girc.Client, e girc.Event) {
-		c.Cmd.Join(viper.GetString("irc.channel"))
-		// Set IRC client as ready
-		i.isReady = true
-	})
+func StartIRC(c *Context) {
+	c.irc.Handlers.Add(girc.CONNECTED, c.joinHandlerIRC)
 
-	i.client.Handlers.Add(girc.TOPIC, func (c *girc.Client, e girc.Event) {
-		log.Println("Topic changed! - Channel: ", e.Params[0], ". Topic: ", e.Last())
-	})
+	c.irc.Handlers.Add(girc.TOPIC, c.topicHandlerIRC)
 
-	log.Print("Connecting to IRC server...")
-	if err := i.client.Connect(); err != nil {
-		log.Fatalf("an error occurred while attempting to connect to %s: %s", i.client.Server(), err)
+	c.irc.Handlers.Add(girc.JOIN, c.inChannelHandlerIRC)
+
+	if err := c.irc.Connect(); err != nil {
+		log.Fatalf("an error occurred while attempting to connect to %s: %s", c.irc.Server(), err)
 	}
 }
 
-func StartDiscord() {
-	log.Print("Starting up Discord...")
+func (context *Context) joinHandlerIRC(c *girc.Client, e girc.Event) {
+	log.Print("Connected to IRC server!")
+	log.Print("Joining IRC channel...")
+	c.Cmd.Join(viper.GetString("irc.channel"))
+}
 
+func (context *Context) topicHandlerIRC(c *girc.Client, e girc.Event) {
+	log.Print("Topic changed in IRC!")
+	context.discord.ChannelMessageSend(context.channelMap[e.Params[0]], "Topic changed in IRC!")
+}
+
+func (context *Context) inChannelHandlerIRC(c *girc.Client, e girc.Event) {
+	if(e.Source.Name == c.GetNick()) {
+		log.Print("Joined Channel!")
+		context.wg.Done()
+	}
+}
+
+func createDiscord() *discordgo.Session {
 	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + viper.GetString("discord.token"))
 
 	if err != nil {
 		log.Fatalf("Error creating Discord session,", err)
-		return
 	}
+	return dg
+}
 
-	d := &discordClient {
-		session: dg,
-		isReady: false,
-	}
-
+func StartDiscord(c *Context) {
 	// Register the messageCreate func as a callback for ChannelUpdate events.
-	d.session.AddHandler(func(s *discordgo.Session, u *discordgo.ChannelUpdate) {
-		log.Print("Channel Update in ", u.Name, ": ", u.Topic)
-		// check the event channel name against the one in the config
-		// then start the mutex topic update
-	})
+	c.discord.AddHandler(c.channelUpdateHandlerDiscord)
 
 	// Callback to handle the ready event when the bot is connected
-	d.session.AddHandler(func(s *discordgo.Session, m *discordgo.Ready) {
-		// set Discord client as ready
-		d.isReady = true
-	})
+	c.discord.AddHandler(c.clientReadyHandlerDiscord)
 
 	log.Print("Connecting to Discord server...")
 	// Open a websocket connection to Discord and begin listening.
-	err = d.session.Open()
+	err := c.discord.Open()
 	if err != nil {
 		log.Fatalf("Error opening connection to Discord,", err)
-		return
 	}
+}
+
+func (context *Context) channelUpdateHandlerDiscord(s *discordgo.Session, u *discordgo.ChannelUpdate) {
+	log.Print("Channel Update in ", u.Name, ": ", u.Topic)
+	// check the event channel name against the one in the config
+	// then start the mutex topic update
+	log.Print("Channel ID: ", u.ID, " Paired with: ", context.channelMap[u.ID])
+	context.irc.Cmd.Message(context.channelMap[u.ID], "Topic changed in Discord!")
+}
+
+func (context *Context) clientReadyHandlerDiscord(s *discordgo.Session, u *discordgo.Ready) {
+	// set Discord client as ready
+	log.Print("Discord client is ready.")
+	context.wg.Done()
 }
 
 func SetupConfig() {
@@ -116,29 +111,45 @@ func SetupConfig() {
 	log.Print("Reading configuration file...")
 	err := viper.ReadInConfig()
 	if err != nil {
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+		log.Fatalf("Fatal error config file: %s \n", err)
 	}
 }
 
-func main() {
-	log.Print("Starting the service...")
+func setupChannelMap() map[string]string {
+	// populate the channel connection mapping
+	channelMap := make(map[string]string)
+	// Bi-directional mapping between channels that are paired
+	channelMap[viper.GetString("irc.channel")] = viper.GetString("discord.channelID")
+	channelMap[viper.GetString("discord.channelID")] = viper.GetString("irc.channel")
+	log.Print(channelMap)
+	return channelMap
+}
 
-	// load config from file
+func main() {
+
 	SetupConfig()
 
-	// initialize the topic checker
-	t := &topicInfo{
-		topic: "",
-		timeSet: time.Now(),
+	var wg sync.WaitGroup
+	context := Context {
+		irc: 		createIRC(),
+		discord: 	createDiscord(),
+		channelMap:	setupChannelMap(),
+		wg: 		&wg,
 	}
 
-	log.Print(t.timeSet)
+	context.wg.Add(1)
+	log.Print("Starting IRC...")
+	go StartIRC(&context)
+	log.Print("Waiting on IRC...")
+	context.wg.Wait()
+	log.Print("IRC is connected and ready to go!")
 
-	// start up irc goroutine
-	go StartIRC()
-
-	// start up discord goroutine
-	go StartDiscord()
+	context.wg.Add(1)
+	log.Print("Starting Discord...")
+	go StartDiscord(&context)
+	log.Print("Waiting on Discord...")
+	context.wg.Wait()
+	log.Print("Discord is connected and ready to go!")
 
 	// Halt main() till kill is received
 	log.Print("Program will now run till sigterm is received...")
@@ -152,8 +163,5 @@ func main() {
 	case syscall.SIGTERM:
 		log.Print("Got SIGTERM...")
 	}
-
 	log.Print("The service is shutting down...")
-	// gracefully kill discord & irc goroutines
-	log.Print("Done")
 }
